01d25c789b3caec530e7b7ecfd9ca11a2df52f62
Dex Chen <chienhua@gmail.com>
2019-05-18 00:25:51 +0800

    first revision

---
+++ README.md
@@ -0,0 +1,5 @@
+  Usage: iso2http [options] <iso>
+  
+  Options:
+    -p Port
+      	Port to linsten on (default 8080)
---
+++ main.go
@@ -0,0 +1,45 @@
+// Package main provides ...
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/dex/iso9660"
+)
+
+var port = flag.Int("p", 8080, "`Port` to linsten on")
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s [options] <iso>\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "\nOptions:\n")
+	flag.PrintDefaults()
+}
+
+type isoFs struct {
+	fs *iso9660.FileSystem
+}
+
+func (iso *isoFs) Open(name string) (http.File, error) {
+	return iso.fs.Open(name)
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		usage()
+		os.Exit(1)
+	}
+
+	fs, err := iso9660.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(&isoFs{fs})))
+}
